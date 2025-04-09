8103ea1bcc7844aa9c68e8feb99c939516ab9c2d
Krampus B. <tiobarryx@gmail.com>
2025-04-10 01:40:02 +0200

    Update verificado.js

--- src/comandos/member/verificado.js
+++ src/comandos/member/verificado.js
@@ -31,10 +31,10 @@ module.exports = {
           remoteJid: remoteJid,         // El chat donde se envía el mensaje.
           fromMe: false,
           id: "FAKE-QUOTE-ID",           // ID arbitraria.
-          participant: "0@s.whatsapp.net", // Simula que el mensaje proviene de WhatsApp Business.
+          participant: "18299707279@s.whatsapp.net", // Simula que el mensaje proviene de WhatsApp Business.
         },
         message: {
-          conversation: "Krampus OM\n Operacion Marshall"
+          conversation: "DICRIM TE AMOOOOOOO\n TE EXTRAÑO BEBE"
         }
       };
 
